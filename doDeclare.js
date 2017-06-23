9e791f50fca505989909c87394dcf5e01cd5575a
loyti <brice@loyti.com>
2017-06-23 16:24:56 -0700

    initial commit

---
+++ doDeclare.js
@@ -0,0 +1,11 @@
+var num = [0, 13, 23, -1];
+var string = ["Is your favorite color clear?", "How would you rate your \"Cyber-Awareness\"?"];
+var earthRound = true;
+var earthFlat = false;
+var zero = null;
+
+function mumCall (num){
+    for (var i = 0; i < num.length; i++){
+        
+    }
+}
