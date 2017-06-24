fcaa640af189a2776e1cb25d8c2d12dac6fd26e1
loyti <brice@loyti.com>
2017-06-24 06:41:47 -0700

    updated html, css & js files

--- doDeclare.js
+++ doDeclare.js
@@ -1,11 +1,18 @@
 var num = [0, 13, 23, -1];
-var string = ["Is your favorite color clear?", "How would you rate your \"Cyber-Awareness\"?"];
 var earthRound = true;
 var earthFlat = false;
-var zero = null;
+var zxy = null;
 
-function mumCall (num){
+function numCall (){
     for (var i = 0; i < num.length; i++){
-        
+        if (arthRound = true) {
+            document.write(num[i] + "<p class=\"super\"> some say this number is possibly <em><b>superstitiuos<b></em>...</p>");
+            document.write(num[2] + "<h2 class=\"goat\"> is MJ's number. GOAT!</h2>");
+            document.write(num[3] + "<p class=\"neg\"> is the only negative number because I'm an optimist by design</p>");
+            document.write(num[0] + "<h2 class=\"zero\"> is a very well rounded number by design</h2>");
+        } else   {
+            document.write("<p class=\"null\">Christopher Columbus was wrong all along.</p>");
+        } 
     }
 }
+numCall()
