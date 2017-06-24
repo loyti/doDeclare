1477fc0b8e4cdd489add2a6936898994c5ae8529
loyti <brice@loyti.com>
2017-06-24 06:45:10 -0700

    updated js file

--- doDeclare.js
+++ doDeclare.js
@@ -5,12 +5,12 @@ var zxy = null;
 
 function numCall (){
     for (var i = 0; i < num.length; i++){
-        if (arthRound = true) {
+        if (earthRound == true) {
             document.write(num[i] + "<p class=\"super\"> some say this number is possibly <em><b>superstitiuos<b></em>...</p>");
             document.write(num[2] + "<h2 class=\"goat\"> is MJ's number. GOAT!</h2>");
             document.write(num[3] + "<p class=\"neg\"> is the only negative number because I'm an optimist by design</p>");
             document.write(num[0] + "<h2 class=\"zero\"> is a very well rounded number by design</h2>");
-        } else   {
+        } else  {
             document.write("<p class=\"null\">Christopher Columbus was wrong all along.</p>");
         } 
     }
