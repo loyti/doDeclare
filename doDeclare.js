229f79fb1f6993c108af037e6a09ce6935ab1029
loyti <brice@loyti.com>
2017-06-26 08:59:18 -0700

    updated html css & js files

--- doDeclare.js
+++ doDeclare.js
@@ -1,18 +1,18 @@
 var num = [0, 13, 23, -1];
-var earthRound = true;
-var earthFlat = false;
+var numClass = ["super", "round", "goat", "neg"]
+var pairNum = [" is a very well rounded number by design</p>"," some say this number is possibly <em><b>superstitiuos</b></em>...</p>", "is MJ's number. GOAT!</p>", " is the only negative number because I'm an optimist by design</p>"];
+var earthRound;
+var earthFlat;
 var zxy = null;
-
+        
 function numCall (){
-    for (var i = 0; i < num.length; i++){
-        if (earthRound == true) {
-            document.write(num[i] + "<p class=\"super\"> some say this number is possibly <em><b>superstitiuos<b></em>...</p>");
-            document.write(num[2] + "<h2 class=\"goat\"> is MJ's number. GOAT!</h2>");
-            document.write(num[3] + "<p class=\"neg\"> is the only negative number because I'm an optimist by design</p>");
-            document.write(num[0] + "<h2 class=\"zero\"> is a very well rounded number by design</h2>");
-        } else  {
+    earthRound = true;
+    if (earthRound == true) {
+        for (var i = 0; i < num.length; i++){
+            document.write(num[i] + "<p class=\"" + numClass + "\">" + pairNum[i]);
+            }
+    } else  {
             document.write("<p class=\"null\">Christopher Columbus was wrong all along.</p>");
-        } 
     }
 }
 numCall()
